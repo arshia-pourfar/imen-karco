d088cedd2230cfb77c2e410b512891171b8adf9b
arshia-pourfar <87662074+arshia-pourfar@users.noreply.github.com>
2023-01-02 14:47:20 +0330

    Add files via upload

---
+++ scripts/product-list.js
@@ -0,0 +1,291 @@
+let boxContainer = [
+    {
+        whatBox: "shert",
+        whatTitleBox: "لباس های <br> ایمن کارکو",
+    },
+    {
+        whatBox: "hat",
+        whatTitleBox: "کلاه های <br> ایمن کارکو",
+    },
+    {
+        whatBox: "shoose",
+        whatTitleBox: "کفش های <br> ایمن کارکو",
+    },
+]
+
+let allProduct = [
+    // shert Products 
+    {
+        id: 0,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-3.jpg"
+    },
+    {
+        id: 1,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-4.jpg"
+    },
+    {
+        id: 2,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-5.jpg"
+    },
+    {
+        id: 3,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-6.jpg"
+    },
+    {
+        id: 4,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-7.jpg"
+    },
+    {
+        id: 5,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-5.jpg"
+    },
+    {
+        id: 6,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-6.jpg"
+    },
+    {
+        id: 7,
+        category: "shert",
+        title: "لباس تستی",
+        imgSrc: "images/products/sherts/product-image-7.jpg"
+    },
+
+    // hat Products 
+    {
+        id: 8,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 9,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 10,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 11,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 12,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 13,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 14,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+    {
+        id: 15,
+        category: "hat",
+        title: "کلاه تستی",
+        imgSrc: "images/products/hats/product-image-1.jpg"
+    },
+
+    // shoose Products
+    {
+        id: 16,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: 'images/products/shoose/product-image-18.jpeg'
+    },
+    {
+        id: 17,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-19.jpeg"
+    },
+    {
+        id: 18,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-20.jpeg"
+    },
+    {
+        id: 19,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-21.jpeg"
+    },
+    {
+        id: 20,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-22.jpeg"
+    },
+    {
+        id: 21,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-23.jpeg"
+    },
+    {
+        id: 22,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-24.jpeg"
+    },
+    {
+        id: 23,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-21.jpeg"
+    },
+    {
+        id: 24,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-23.jpeg"
+    },
+    {
+        id: 25,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-24.jpeg"
+    },
+    {
+        id: 26,
+        category: "shoose",
+        title: "لباس تستی",
+        imgSrc: "images/products/shoose/product-image-21.jpeg"
+    },
+]
+
+let customersLogo = [
+    {
+        id: 0,
+        className: "anata",
+        imgSrc: "images/logo/Anata-logo.png"
+    },
+    {
+        id: 1,
+        className: "kanaf-iran",
+        imgSrc: "images/logo/Kanaf-Iran-logo.png"
+    },
+    {
+        id: 2,
+        className: "sumsung",
+        imgSrc: "images/logo/sumsung-logo.png"
+    },
+    {
+        id: 3,
+        className: "khoshgovar",
+        imgSrc: "images/logo/Khoshgovar-logo.png"
+    },
+    {
+        id: 4,
+        className: "anata",
+        imgSrc: "images/logo/Parsiangostar-logo.png"
+    },
+    {
+        id: 5,
+        className: "pouyesh-pajouhan",
+        imgSrc: "images/logo/Pouyesh-pajouhan-logo.png"
+    },
+    {
+        id: 6,
+        className: "abidi",
+        imgSrc: "images/logo/Abidi-logo.png"
+    },
+    {
+        id: 7,
+        className: "akhavan",
+        imgSrc: "images/logo/Parskhazar-logo.png"
+    },
+    {
+        id: 8,
+        className: "parskhazar",
+        imgSrc: "images/logo/Akhavan-logo.png"
+    },
+    {
+        id: 9,
+        className: "ramak",
+        imgSrc: "images/logo/Ramak-logo.png"
+    },
+    {
+        id: 10,
+        className: "casterol",
+        imgSrc: "images/logo/Casterol-logo.png"
+    },
+    {
+        id: 11,
+        className: "goushtiran",
+        imgSrc: "images/logo/Goushtiran-logo.png"
+    },
+    {
+        id: 12,
+        className: "snowa",
+        imgSrc: "images/logo/Snowa-logo.png"
+    },
+    {
+        id: 13,
+        className: "parssimin",
+        imgSrc: "images/logo/Parssimin-logo.png"
+    },
+]
+
+let whyus = [
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+    {
+        postTitle: "دوخت تضمینی",
+        imgSrc: "images/icons/thread-icon.png",
+        content: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+    },
+]
+
---
+++ scripts/script.js
@@ -0,0 +1,176 @@
+$(document).ready(function () {
+    if (window.location.href.indexOf("index.html") > -1) {
+        (function () {
+            for (let i = 0; i < boxContainer.length; i++) {
+                document.querySelector(".Products .row").innerHTML += `
+                <div class="`+ boxContainer[i].whatBox + ` col-11 container" data-aos="zoom-in-up" data-aos-duration="2000">
+                    <div class="row">
+                        <div class="about-container-post col-md-2 col-3">
+                            <span class="text col-12">`+ boxContainer[i].whatTitleBox + `</span>
+                            <a href="#" class="show-more col-12">مشاهده همه<i class="fas fa-arrow-left"></i></a>
+                        </div>
+                        <div class="post-container col-md-10 col-9">
+                            <button class="scroll-left-post" id="scroll-left-post-`+ boxContainer[i].whatBox + `">
+                                <i class="fas fa-arrow-left"></i></button>
+                            <button class="scroll-right-post " id="scroll-right-post-`+ boxContainer[i].whatBox + `">
+                                <i class="fas fa-arrow-right"></i></button>
+                            <div class="post" id="box-post-`+ boxContainer[i].whatBox + `"></div>
+                        </div>
+                    </div>
+                </div>`;
+                if (i == boxContainer.length - 1) {
+                    addProductToBox();
+                }
+            }
+        }());
+    }
+
+    $(".click-home-page").click(function () {
+        window.location = "index.html";
+    });
+
+    $(".scroll-to-product").click(function () {
+        scrollToItemSelect("product");
+    });
+
+    $(".click-info").click(function () {
+        scrollToItemSelect("footer");
+    });
+
+    $(".click-phone").click(function () {
+        scrollToItemSelect("footer");
+    });
+
+    $(".menu-btn").click(function () {
+        $(".menu-btn").css("display", "none");
+        $(".slide-nav").addClass('col-6');
+        $(".slide-nav").show(800);
+        $(".slide-nav .nav").css("display", "block");
+    });
+
+    $(".close-slide-nav").click(function () {
+        $(".slide-nav .nav").css("display", "none");
+        $(".slide-nav").hide(600);
+        $(".slide-nav").removeClass('col-6');
+        $(".menu-btn").css("display", "block");
+    });
+
+    $("#scroll-left-post-shert").click(function () {
+        checkScrollBox("shert", "left");
+    });
+
+    $("#scroll-right-post-shert").click(function () {
+        checkScrollBox("shert", "right");
+    });
+
+    $("#scroll-left-post-hat").click(function () {
+        checkScrollBox("hat", "left");
+    });
+
+    $("#scroll-right-post-hat").click(function () {
+        checkScrollBox("hat", "right");
+    });
+
+    $("#scroll-left-post-shoose").click(function () {
+        checkScrollBox("shoose", "left");
+    });
+
+    $("#scroll-right-post-shoose").click(function () {
+        checkScrollBox("shoose", "right");
+    });
+
+    $("#footer .item-about-us .read-more p").click(function () {
+        $("#footer .item-about-us .read-more p").css("visibility", "hidden");
+        $("#footer .item-about-us .read-more").css("visibility", "hidden");
+        $("#footer .fade-text").css("visibility", "hidden");
+    });
+
+    $(".close-container-product-secifications").click(function () {
+        $(".bg-container-product-secifications").fadeOut(700);
+
+    })
+
+});
+
+let lastScrollSize, oneToTheLastScrollSize;
+function checkScrollBox(product, direction) {
+    let oppositeDirection, minusOrPlusLeftScroll;
+    if (direction == "left") {
+        oppositeDirection = "right";
+        minusOrPlusLeftScroll = "-";
+
+    } else if (direction == "right") {
+        oppositeDirection = "left";
+        minusOrPlusLeftScroll = "+";
+    }
+    $("#box-post-" + product + "").animate({
+        scrollLeft: "" + minusOrPlusLeftScroll + "=250"
+    }, 10, function () {
+        lastScrollSize = $("#box-post-" + product + "").scrollLeft();
+        if (lastScrollSize == oneToTheLastScrollSize) {
+            $("#scroll-left-post-" + product + "").css("visibility", "hidden");
+        } else if (lastScrollSize != oneToTheLastScrollSize) {
+            oneToTheLastScrollSize = lastScrollSize;
+        }
+        if ($("#box-post-" + product + "").scrollLeft() >= -0.9) {
+            oneToTheLastScrollSize = -0.9;
+            $("#scroll-right-post-" + product + "").css("visibility", "hidden");
+        }
+        $("#scroll-" + oppositeDirection + "-post-" + product + "").css("visibility", "visible");
+    });
+}
+
+function addProductToBox() {
+    for (let selectProducts = 0; selectProducts < allProduct.length; selectProducts++) {
+        document.getElementById("box-post-" + allProduct[selectProducts].category + "").innerHTML += `
+            <article onclick="getIdItemClicked(`+ allProduct[selectProducts].id + `);" class="post-item col-lg-2 col-md-3 col-4">
+                <div class="post-image">
+                    <img src="`+ allProduct[selectProducts].imgSrc + `" alt="">
+                </div>
+                <div class="post-content">
+                    <p>`+ allProduct[selectProducts].title + `</p>
+                </div>
+            </article>`
+    }
+    for (let i = 0; i < customersLogo.length; i++) {
+        document.querySelector(".logo-image").innerHTML += `
+            <img class="col-lg-1 col-sm-2 col-3 ` + customersLogo[i].className + `" src=" ` + customersLogo[i].imgSrc + `">
+        `;
+    }
+    for (let i = 0; i < whyus.length; i++) {
+        document.querySelector(".posts-container").innerHTML += `
+                <article class="posts col-lg-4 col-md-6 col-6">
+                    <div class="post-item">
+                        <div class="icon-post container">
+                            <div class="row">
+                                <img class="icon col-md-4" src="`+ whyus[i].imgSrc + `">
+                                <div class="title-post col-md-8 col-12">`+ whyus[i].postTitle + `</div>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <p>`+ whyus[i].content + `</p>
+                        </div>
+                    </div>
+                </article>
+        `;
+    }
+}
+
+function scrollToItemSelect(item) {
+    if (window.location.href.indexOf("index.html") > -1) {
+        let elementScroll = document.getElementById(item).offsetTop;
+        if (item == "product") {
+            window.scrollTo({ top: elementScroll + 70, behavior: 'smooth' });
+        } else {
+            window.scrollTo({ top: elementScroll - 50, behavior: 'smooth' });
+        }
+    } else {
+        window.location = "index.html";
+        scrollToItemSelect(item);
+    }
+}
+
+function getIdItemClicked(id) {
+    $(".bg-container-product-secifications").fadeIn(800);
+    $(".container-product-secifications .product-secifications .title").text(allProduct[id].title);
+}
